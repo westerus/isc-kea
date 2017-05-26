ae6039631741e76e34ee81e5ef11fc332d31ee33
Ernesto Pérez <westerus@gmail.com>
2017-05-26 14:55:23 +0200

    Add config file for dhcp4 and dhcp6
    Config files an dockerfile for services dhcp

--- dhcpv4/rootfs/entry-point.sh
+++ dhcpv4/rootfs/entry-point.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
 set -e
-## COMMANDS... ##
+KEACONF=/etc/kea/kea.conf
+
+check_config () {
+if [ -f $KEACONF ]; then
+  /usr/bin/kea-dhcp4 -c $KEACONF
+  if [ ! -z $? ]; then
+    echo "Error in kea config file $KEACONF"
+    exit 1
+  fi
+fi
+}
+
+check_config
+
 exec "$@"
