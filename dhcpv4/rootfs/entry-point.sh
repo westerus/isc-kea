b5af33c9aa30a82374d9d34a047f9a9879c6e163
Ernesto Pérez <westerus@gmail.com>
2017-05-29 13:05:25 +0200

    Clean libraries
    Delete libraries

--- dhcpv4/rootfs/entry-point.sh
+++ dhcpv4/rootfs/entry-point.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 set -e
 KEACONF=/etc/kea/kea.conf
+KEABIN=/usr/bin/kea-dhcp4
 
 check_config () {
 if [ -f $KEACONF ]; then
-  /usr/bin/kea-dhcp4 -t $KEACONF
-  if [ ! -z $? ]; then
-    echo "Error in kea config file $KEACONF"
+  $KEABIN -t $KEACONF
+  if [ $? -eq 0 ]; then
+    echo "Check file $KEACONF [OK]"
+  else
+    echo "Error in test kea file $KEACONF"
     exit 1
   fi
 fi
